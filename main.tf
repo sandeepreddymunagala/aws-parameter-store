a5189b27175826f6a6f48faa091c0fcf28451091
sandeep <munagalasandeepreddy@gamail.com>
2024-05-15 09:36:22 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ variable "params" {
     ### passwords will be never part of git repos
     {name = "roboshop.dev.payment.amqp_pass", type = "SecureString",value = "roboshop123"},
     {name = "roboshop.dev.dispatch.amqp_pass", type = "SecureString",value = "roboshop123"},
-    {name = "roboshop.dev.rabbitmq.amqp_pass", type = "SecureString",value = "roboshop123"}
-    {name = "roboshop.dev.mysql.password", type = "SecureString",value = "roboshop123"}
+    {name = "roboshop.dev.rabbitmq.amqp_pass", type = "SecureString",value = "roboshop123"},
+    {name = "roboshop.dev.mysql.password", type = "SecureString",value = "roboshop123"},
   ]
 }
