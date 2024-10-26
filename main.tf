40fb9494cfe769a6042841417f0ade10e8144c69
sandeep <munagalasandeepreddy@gamail.com>
2024-10-26 13:03:03 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ resource "aws_ssm_parameter" "parameters" {
 
 variable "params" {
   default = [
-    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.sandeepreddymunagala123.xyz:8080/"},
-    {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.sandeepreddymunagala123.xyz:8080/"},
-    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.sandeepreddymunagala123.xyz:8080/"},
-    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.sandeepreddymunagala123.xyz:8080/"},
-    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.sandeepreddymunagala123.xyz:8080/"}
+    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.sandeepreddymunagala123.xyz:8080/",type ="String"},
+    {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.sandeepreddymunagala123.xyz:8080/",type ="String"},
+    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.sandeepreddymunagala123.xyz:8080/",type ="String"},
+    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.sandeepreddymunagala123.xyz:8080/",type ="String"},
+    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.sandeepreddymunagala123.xyz:8080/",type ="String"}
 
    ]
 }
